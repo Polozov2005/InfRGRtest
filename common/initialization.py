f2ebc57f7fcf346453a48ea221ef89e00e5aa7bb
Polozov2005 <aleksandr.polozov.2005@gmail.com>
2024-05-09 09:09:34 +0700

    1

--- common/initialization.py
+++ common/initialization.py
@@ -8,41 +8,57 @@ import numpy as np # Для создания матриц
 def E():
     filepath = 'excell_tables/E.xlsx'
     df = pd.read_excel(filepath)
+
     rowcount = df.shape[0]
+
     list = np.zeros([rowcount + 1], dtype=np.complex64)
+
     for i in range(1, rowcount + 1):
         list[i] = df.iloc[i - 1, 1]
+
     matrix = np.zeros([30, 1], dtype=np.complex64)
+
     matrix[0,  0] = list[1] # E1 -> Y1
     matrix[1,  0] = list[2] # E2 -> Y2
     matrix[16, 0] = list[3] # E3 -> Y17
     matrix[17, 0] = list[4] # E4 -> Y18
+
     dictionary = {
         'list':list,
         'matrix':matrix
     }
+
     return dictionary
 
 # Инициализация значений проводимости из файла Y.xlsx
 def Y():
     filepath = 'excell_tables/Y.xlsx'
     df = pd.read_excel(filepath)
+
     rowcount = df.shape[0]
+
     real = np.zeros([rowcount + 1], dtype=np.complex64)
     imag = np.zeros([rowcount + 1], dtype=np.complex64)
+
     for i in range(1, rowcount + 1):
         real[i] = df.iloc[i - 1, 1]
+
     for i in range(1, rowcount + 1):
         imag[i] = df.iloc[i - 1, 2]
     imag = imag * np.sqrt(-1, dtype=np.complex64)
+
     list = real + imag
+
     matrix = np.zeros([30, 30], dtype=np.complex64)
+
     for i in range(matrix.shape[0]):
         matrix[i, i] = list[i + 1]
+
     dictionary = {
         'list':list,
         'matrix':matrix
     }
+
     return dictionary
 
 # Инициализация матрицы соединений
@@ -50,11 +66,14 @@ def A():
     filepath = 'excell_tables/A.xlsx'
     df = pd.read_excel(filepath)
     df = df.fillna(0)
+
     matrix = df.iloc[0: , 1:].values
     matrix = matrix.astype('complex64')
+
     dictionary = {
         'matrix':matrix
     }
+    
     return dictionary
 
 # Инициализация значений
--- common/matrix_expression.py
+++ common/matrix_expression.py
@@ -62,18 +62,67 @@ def transposition(A):
 
     return result
 
-A = np.array([
-    [1, 1],
-    [1, 1],
-    [1, 1],
-    [1, 1],
+# Решение СЛАУ методом Гаусса
+def gauss(A, B):
 
+    if A.shape[0] != A.shape[1]:
+        print('Левая часть уравнения должна быть квадратной матрицей')
+    elif B.shape[1] != 1:
+        print('Правая часть уравнения должна быть столбцом')
+    elif A.shape[0] != B.shape[0]:
+        print('Количества строк левой и правой матриц должны совпадать')
+
+    else:
+        C = np.copy(A)
+        C = C.astype('complex64')
+
+        D = np.copy(B)
+        D = D.astype('complex64')
+
+        rowcount = C.shape[0]
+
+        X = np.zeros([rowcount, 1], dtype=np.complex64)
+
+        # Прямой ход
+        for k in range(rowcount - 1):
+            if C[k, k] == 0:
+                print('В ходе решения на главной диагонали оказался ноль - система не совместна')
+            else:
+                for i in range(k+1, rowcount):
+                    coefficient = C[i, k] / C[k, k]
+                    C[i, k] = 0
+
+                    for j in range(k+1, rowcount):
+                        C[i, j] += -C[k, j] * coefficient
+                    
+                    D[i] += -D[k] * coefficient
+
+        # Обратный ход
+        X[rowcount - 1] = D[rowcount - 1] / C[rowcount - 1, rowcount - 1]
+        for i in range(rowcount - 2, 0 - 1, -1):
+            S = 0
+
+            for j in range(i+1, rowcount):
+                S += C[i, j] * X[j]
+            
+            X[i] = (D[i] - S) / C[i, i]
+
+
+        result = X
+        return result
+
+A = np.array([
+    [5, 1 + 1j, 4],
+    [1, 2, 6],
+    [6, 4, 3],
 ])
 
 B = np.array([
-    [2],
-    [2]
+    [3],
+    [4],
+    [7],
+    [8]
 ])
 
-C = transposition(A)
-print(C)
+print(gauss(A, B))
+print(np.linalg.solve(A, B))
